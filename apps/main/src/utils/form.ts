633eb98fd5a388a9cc4b8819d44ea0ac453b538b
Thomas Ramé <thomas.rame@outlook.com>
2023-01-25 09:11:52 +0100

    wip

--- apps/main/src/utils/form.ts
+++ apps/main/src/utils/form.ts
@@ -1,4 +1,7 @@
 import { TypographyProps } from '@mui/material';
+import { useConfirm } from 'material-ui-confirm';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { RegisterOptions } from 'react-hook-form';
 
 export function forceHtmlRadioOutputToBeBoolean(value: string | null): boolean | null {
@@ -24,3 +27,38 @@ export const formTitleProps: TypographyProps<'h1'> = {
     mb: 2,
   },
 };
+
+// Since Next 13 it's not that easy to prevent leaving page from router hooks...
+// Read the below link and give it a try... no sure 100% it can prevent navigation (except if wrapping all NextLink components + router.push)
+// https://github.com/vercel/next.js/discussions/41745#discussioncomment-3987025
+export function useWarnLeavingDirtyForm(isDirty: boolean) {
+  const router = useRouter();
+  const askConfirmation = useConfirm();
+
+  useEffect(() => {
+    const warningText = 'Vous avez faits des modifications qui ne sont pas enregistrées, êtes-vous sûr de vouloir quitter cette page ?';
+
+    const handleWindowClose = (e: BeforeUnloadEvent) => {
+      if (!isDirty) return;
+
+      e.preventDefault();
+
+      return (e.returnValue = warningText);
+    };
+    const handleBrowseAway = () => {
+      if (!isDirty) return;
+
+      if (window.confirm(warningText)) return;
+
+      router.events.emit('routeChangeError');
+      throw 'routeChange aborted';
+    };
+
+    window.addEventListener('beforeunload', handleWindowClose);
+    router.events.on('routeChangeStart', handleBrowseAway);
+    return () => {
+      window.removeEventListener('beforeunload', handleWindowClose);
+      router.events.off('routeChangeStart', handleBrowseAway);
+    };
+  }, [isDirty]);
+}
